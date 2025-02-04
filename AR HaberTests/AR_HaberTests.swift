72a49b2e87d85630a6df5221fab3f3089ee55e7d
arenkos <arenkos24@gmail.com>
2025-02-05 01:16:25 +0300

    Initial Commit

---
+++ AR HaberTests/AR_HaberTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_HaberTests.swift
+//  AR HaberTests
+//
+//  Created by Aren Koş on 5.02.2025.
+//
+
+import Testing
+@testable import AR_Haber
+
+struct AR_HaberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
